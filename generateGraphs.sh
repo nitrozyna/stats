17c08a3d4626359f5e3abbe1b178df2b93a41587
Adam Kurkiewicz <adam@kurkiewicz.pl>
2015-11-21 19:10:30 +0000

    Data for male samples and prettier graphs

---
+++ generateGraphs.sh
@@ -0,0 +1,2 @@
+python stats.py menInd/ menDep/ 3
+python stats.py womenInd/ womenDep/ 0
--- stats.py
+++ stats.py
@@ -1,5 +1,7 @@
 import random, scipy.stats
 import matplotlib.pyplot as plt
+import os.path, os, sys
+
 def lowerUpperBoundary(varsy, slackFraction):
     varsyMax = max(varsy)
     varsyMin = min(varsy)
@@ -11,38 +13,43 @@ def bestFitLine(leftBorder, rightBorder, intercept, slope):
     highY = rightBorder * slope + intercept
     return [leftBorder, rightBorder], [lowY, highY]
 
-
 def makePlot(independentVar, dependentVar, independentVarName, dependentVarName, plotname):
+    global figureNo
     slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(independentVar, dependentVar)
 
     leftBorder, rightBorder = lowerUpperBoundary(independentVar, 0.1)
 
     leftBorderLine, rightBorderLine = lowerUpperBoundary(independentVar, 0.05)
 
-    bottomBorder, topBorder = lowerUpperBoundary(dependentVar, 0.1)
+    bottomBorder, topBorder = lowerUpperBoundary(dependentVar, 0.3)
 
     xs, ys = bestFitLine(leftBorderLine, rightBorderLine, intercept, slope)
 
 
 
-    fig = plt.figure()
-    #fig.suptitle('sample correlation', fontsize=12)
+    fig = plt.figure(figsize=(10, 5))
+
+    verticalBar = scipy.std(dependentVar)
+
+    horizontalBar = scipy.std(independentVar)    
 
     ax = fig.add_subplot(111)
-    fig.subplots_adjust(left=0.2, bottom=0.4)
-    ax.set_title('Sample correlation', fontsize=16)
-    d1 = 'best fit line slope: ' + '%.3g' % slope + "\n"
-    d2 = 'best fit line r-value: ' + '%.3g' % r_value + "\n"
-    d3 = 'best fit line p-value: ' + '%.3g' % p_value + "\n"
-    d4 = 'best fit line standard error: ' + '%.3g' % std_err + "\n"
+    fig.subplots_adjust(left=0.15, bottom=0.4, top=0.85)
+    ax.set_title(descriptions[figureNo] + '\n', fontsize=16)
+    figureNo += 1
+    d1 = 'Best fit line slope: ' + '%.3g' % slope + "\n"
+    d2 = "Pearson's coefficient-value: " + '%.3g' % r_value + "\n"
+    d3 = 'Null hypothesis p-value: ' + '%.3g' % p_value + "\n"
     
-    fig.text(0.12, 0.02, d1 + d2 + d3 + d4 , fontsize=16)
+    fig.text(0.12, 0.02, d1 + d2 + d3 , fontsize=16)
     
     ax.set_xlabel(independentVarName, fontsize = 18)
     ax.set_ylabel(dependentVarName, fontsize = 18)
     
-    ax.plot(independentVar, dependentVar, 'ro') # plots all points, red dots
-    ax.plot(xs, ys) # shows line of best fit
+    ax.plot(xs, ys, label='line of best fit') # shows line of best fit
+    #fig.suptitle('sample correlation', fontsize=12)
+    ax.errorbar(independentVar, dependentVar, yerr=verticalBar, fmt='go', label='individuals')
+    ax.legend(loc=(0.74, -0.72), shadow=True, fancybox=True)
     ax.axis([leftBorder, rightBorder, bottomBorder, topBorder]) # creates canvas
 
     if slope < 0:
@@ -52,7 +59,10 @@ def makePlot(independentVar, dependentVar, independentVarName, dependentVarName,
 
     plt.savefig(plotname)
 
-import os.path, os, sys
+
+figureNo = int(sys.argv[3])
+descriptions = ["Correlation between BMI and Energy Balance in females", "Correlation between Body Fat and Energy Balance in females", "Correlation between Waist to Hip Ratio and Energy Balance in females"]
+descriptions += [stringa[:-7] + "males" for stringa in descriptions]
 
 dependentDir = sys.argv[2]
 
@@ -66,18 +76,23 @@ def allFilenames(dirname):
 def processFile(absoluteFilePath):
     variables = []
     with open(absoluteFilePath) as myFile:
+        print(absoluteFilePath)
         for index, line in enumerate(myFile):
-            print(index, line)
             if index == 0:
                 varName = str(line)
                 isFirst = False
             else:
                 variables.append(float(line))
     return varName, variables
-            
-for indFilename, indRel in allFilenames(independentDir):
+
+indFilenames = list(allFilenames(independentDir))
+indFilenames.sort()
+depFilenames = list(allFilenames(dependentDir))
+depFilenames.sort()
+
+for indFilename, indRel in indFilenames:
     indName, indVar = processFile(indFilename)
-    for depFilename, depRel in allFilenames(dependentDir):
+    for depFilename, depRel in depFilenames:
         depName, depVar = processFile(depFilename)
         makePlot(indVar, depVar, indName, depName, os.path.join("plots", depRel + indRel + ".png"))
 
